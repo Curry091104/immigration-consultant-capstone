bfdff90a444794b2e5ebcf3f24c370c00acab781
Tuong Nguyen Pham <npham50@my.centennialcollege.ca>
2025-03-19 09:36:04 -0400

    Update crs_links_agent.py

--- backend/controllers/agents/crs_links_agent.py
+++ backend/controllers/agents/crs_links_agent.py
@@ -25,8 +25,24 @@ class CRSLinksAgent:
     def get_recommendations(self, user_input):
         process_input = self.preprocess_input(user_input)
         recommendation = {}
-        for keyword in process_input:
-            for item in crs_links['Comprehensive Ranking System (CRS)']:
-                if keyword in item['keywords']:
-                    recommendation[item['title']] = item['url']        
+        max_matches = 0
+        best_match = None
+        
+        for item in crs_links["Comprehensive Ranking System (CRS)"]:
+            matches = 0
+            for keyword in item["keywords"]:
+                if keyword in process_input:
+                    matches += 1
+            if matches > max_matches:
+                max_matches = matches
+                best_match = item
+        if best_match:
+            recommendation[best_match["title"]] = best_match["url"]
         return recommendation if recommendation else "No recommendations found"
+    
+    
+################## TESTING ##################
+if __name__ == "__main__":
+    agent = CRSLinksAgent()
+    user_input = "What is the criteria of Comprehensive Ranking System (CRS)?"
+    print(agent.get_recommendations(user_input))
