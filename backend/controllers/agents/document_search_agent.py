576b1739946d555bb7fe21d5917dc649ec46d380
Tuong Nguyen Pham <npham50@my.centennialcollege.ca>
2025-03-21 08:46:40 -0400

    update backend

--- backend/controllers/agents/document_search_agent.py
+++ backend/controllers/agents/document_search_agent.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.join(os.getcwd()))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from config.mypinecone import MyPinecone
 import json
